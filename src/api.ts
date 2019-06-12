f312413140a3aa432262bd144a856bc46ab3155a
Thomas Rory Gummerson <thomas@gummerson.no>
2019-06-12 22:01:49 +0200

    remove useless comments

--- src/api.ts
+++ src/api.ts
@@ -13,20 +13,3 @@ export const validate = (token: string, baseURL = defaultBaseURL) =>
 	})
 		.then(res => res.data)
 		.catch(() => false);
-/*
-axios.post(baseURL + '/login', {
-	email: 'trgwii@hotmail.com',
-	password: 'gonnagetyou'
-})
-	.then(res => res.data)
-	.then(token => {
-		axios(baseURL + '/validate', {
-			headers: {
-				Authorization: 'jwt ' + 'f' + token.slice(1),
-				Cookie: 'AccessToken=' + token + '; Email=trgwii@hotmail.com'
-			}
-		})
-			.then(res => res.data)
-			.then(console.log);
-	});
-*/
