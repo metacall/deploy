f905d309782042d98902417352c72b6502d7b93e
Creatoon <yasharyan1099@gmail.com>
2023-01-03 06:28:54 +0530

    refactored some code and handled absence of local faas

--- src/cli/unknown.ts
+++ src/cli/unknown.ts
@@ -5,10 +5,12 @@ import { warn } from './messages';
 export const handleUnknownArgs = (): void => {
 	const flags = args['_unknown'];
 
-	if (flags.length === 1 && (flags[0] === '--help' || flags[0] === '-h'))
-		printHelp();
+	if (!(flags.length === 1 && (flags[0] === '--help' || flags[0] === '-h'))) {
+		const message = `${flags.join(
+			', '
+		)} does not exists as a valid command.`;
+		warn(message);
+	}
 
-	const message = `${flags.join(', ')} does not exist.`;
-	warn(message);
 	printHelp();
 };
