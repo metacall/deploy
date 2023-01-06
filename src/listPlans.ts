aa673541da5ea1f330ae51a8616bd2cdf85fdfdf
Creatoon <yasharyan1099@gmail.com>
2023-01-06 12:19:58 +0530

    Neglected plans for --dev

--- src/listPlans.ts
+++ src/listPlans.ts
@@ -1,11 +1,18 @@
-import { API as APIInterface } from '@metacall/protocol/protocol';
-import { info } from './cli/messages';
+import {
+	API as APIInterface,
+	ProtocolError
+} from '@metacall/protocol/protocol';
+import { apiError, info } from './cli/messages';
 import { planFetch } from './plan';
 
 export const listPlans = async (api: APIInterface): Promise<void> => {
-	const availPlans = await planFetch(api);
+	try {
+		const availPlans = await planFetch(api);
 
-	Object.keys(availPlans).forEach(el => {
-		info(`${el} : ${availPlans[el]}`);
-	});
+		Object.keys(availPlans).forEach(el => {
+			info(`${el} : ${availPlans[el]}`);
+		});
+	} catch (err) {
+		apiError(err as ProtocolError);
+	}
 };
