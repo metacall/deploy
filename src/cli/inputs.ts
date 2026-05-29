080400216af4bdf8307f1ab31a5493e3d7a76633
HyperDanisH <hypertxt.assin@gmail.com>
2026-05-29 12:33:54 +0530

    refactor: Use inuirer.js@v10

--- src/cli/inputs.ts
+++ src/cli/inputs.ts
@@ -1,20 +1,24 @@
-import { prompt } from 'inquirer';
+import inquirer from 'inquirer';
 
 export const input = (message: string): Promise<string> =>
-	prompt<{ data: string }>([
-		{
-			type: 'input',
-			name: 'data',
-			message
-		}
-	]).then(res => res.data);
+	inquirer
+		.prompt<{ data: string }>([
+			{
+				type: 'input',
+				name: 'data',
+				message
+			}
+		])
+		.then(res => res.data);
 
 export const maskedInput = (message: string): Promise<string> =>
-	prompt<{ data: string }>([
-		{
-			type: 'password',
-			name: 'data',
-			message,
-			mask: '*'
-		}
-	]).then(res => res.data);
+	inquirer
+		.prompt<{ data: string }>([
+			{
+				type: 'password',
+				name: 'data',
+				message,
+				mask: '*'
+			}
+		])
+		.then(res => res.data);
--- src/cli/selection.ts
+++ src/cli/selection.ts
@@ -4,77 +4,89 @@ import {
 	Languages
 } from '@metacall/protocol/language';
 import { Plans } from '@metacall/protocol/plan';
-import { prompt } from 'inquirer';
+import inquirer from 'inquirer';
 
 export const loginSelection = (methods: string[]): Promise<string> =>
-	prompt<{ method: string }>([
-		{
-			type: 'list',
-			name: 'method',
-			message: 'Select the login method',
-			choices: methods
-		}
-	]).then((res: { method: string }) => res.method);
+	inquirer
+		.prompt<{ method: string }>([
+			{
+				type: 'list',
+				name: 'method',
+				message: 'Select the login method',
+				choices: methods
+			}
+		])
+		.then((res: { method: string }) => res.method);
 
 export const fileSelection = (
 	message: string,
 	files: string[] = []
 ): Promise<string[]> =>
-	prompt<{ scripts: string[] }>([
-		{
-			type: 'checkbox',
-			name: 'scripts',
-			message,
-			choices: files
-		}
-	]).then((res: { scripts: string[] }) => res.scripts);
+	inquirer
+		.prompt<{ scripts: string[] }>([
+			{
+				type: 'checkbox',
+				name: 'scripts',
+				message,
+				choices: files
+			}
+		])
+		.then((res: { scripts: string[] }) => res.scripts);
 
 export const languageSelection = (
 	languages: LanguageId[] = []
 ): Promise<LanguageId[]> =>
-	prompt<{ langs: string[] }>([
-		{
-			type: 'checkbox',
-			name: 'langs',
-			message: 'Select languages to run on MetaCall',
-			choices: languages.map(lang => Languages[lang].displayName)
-		}
-	]).then((res: { langs: string[] }) =>
-		res.langs.map(lang => DisplayNameToLanguageId[lang])
-	);
+	inquirer
+		.prompt<{ langs: string[] }>([
+			{
+				type: 'checkbox',
+				name: 'langs',
+				message: 'Select languages to run on MetaCall',
+				choices: languages.map(lang => Languages[lang].displayName)
+			}
+		])
+		.then((res: { langs: string[] }) =>
+			res.langs.map(lang => DisplayNameToLanguageId[lang])
+		);
 
 export const planSelection = (
 	message: string,
 	availablePlans: string[]
 ): Promise<Plans> =>
-	prompt<{ plan: Plans }>([
-		{
-			type: 'list',
-			name: 'plan',
-			message,
-			choices: availablePlans
-		}
-	]).then((res: { plan: Plans }) => res.plan);
+	inquirer
+		.prompt<{ plan: Plans }>([
+			{
+				type: 'list',
+				name: 'plan',
+				message,
+				choices: availablePlans
+			}
+		])
+		.then((res: { plan: Plans }) => res.plan);
 
 export const listSelection = (
 	list: string[] | { name: string; value: string }[],
 	message: string
 ): Promise<string> =>
-	prompt<{ container: string }>([
-		{
-			type: 'list',
-			name: 'container',
-			message,
-			choices: list
-		}
-	]).then((res: { container: string }) => res.container);
+	inquirer
+		.prompt<{ container: string }>([
+			{
+				type: 'list',
+				name: 'container',
+				message,
+				choices: list
+			}
+		])
+		.then((res: { container: string }) => res.container);
 
 export const consentSelection = (message: string): Promise<boolean> =>
-	prompt<{ consent: boolean }>([
-		{
-			type: 'confirm',
-			name: 'consent',
-			message,
-			default: false
-		}
-	]).then((res: { consent: boolean }) => res.consent);
+	inquirer
+		.prompt<{ consent: boolean }>([
+			{
+				type: 'confirm',
+				name: 'consent',
+				message,
+				default: false
+			}
+		])
+		.then((res: { consent: boolean }) => res.consent);
